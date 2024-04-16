76e9b587a0f225a39fea9c773f0bf3eab2318c12
Ankika Dey <ankikadey@outlook.com>
2024-04-16 10:47:45 +0530

    Add Two Numbers in C++

---
+++ LeetCode Solutions/2. Add two numbers/addTwoNumbers.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* curr = &dummy;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry > 0) {
+            if (l1 != nullptr) {
+                carry += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != nullptr) {
+                carry += l2->val;
+                l2 = l2->next;
+            }
+            curr->next = new ListNode(carry % 10);
+            carry /= 10;
+            curr = curr->next;
+        }
+
+        return dummy.next;
+    }
+};
+
+int main() {
+    // Example usage
+    ListNode* l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(3);
+
+    ListNode* l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+
+    Solution sol;
+    ListNode* result = sol.addTwoNumbers(l1, l2);
+
+    cout << "Result: ";
+    while (result != nullptr) {
+        cout << result->val << " ";
+        result = result->next;
+    }
+    cout << endl;
+
+    return 0;
+}
