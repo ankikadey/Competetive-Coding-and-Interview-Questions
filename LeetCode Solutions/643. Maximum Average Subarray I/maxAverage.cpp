ce78e9e4bcfd553e65960484aed58907773c3f89
Ankika Dey <ankikadey@outlook.com>
2024-05-31 10:33:30 +0530

    add code

---
+++ LeetCode Solutions/643. Maximum Average Subarray I/maxAverage.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <numeric>
+using namespace std;
+
+class Solution
+{
+public:
+    double findMaxAverage(vector<int> &nums, int k)
+    {
+        double sum = accumulate(nums.begin(), nums.begin() + k, 0);
+        double ans = sum;
+
+        for (int i = k; i < nums.size(); ++i)
+        {
+            sum += nums[i] - nums[i - k];
+            ans = max(ans, sum);
+        }
+
+        return ans / k;
+    }
+};
