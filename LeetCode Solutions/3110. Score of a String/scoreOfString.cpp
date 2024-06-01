e10262561f8b594a6944d7340d8adf4fc2ff678f
Ankika Dey <ankikadey@outlook.com>
2024-06-01 07:36:08 +0530

    add Score of a String in C++

---
+++ LeetCode Solutions/3110. Score of a String/scoreOfString.cpp
@@ -0,0 +1,16 @@
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    int scoreOfString(string s)
+    {
+        int ans = 0;
+
+        for (int i = 1; i < s.length(); ++i)
+            ans += abs(s[i] - s[i - 1]);
+
+        return ans;
+    }
+};
