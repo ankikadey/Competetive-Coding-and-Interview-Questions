6ee7da4bc877e259dcfcf24964a47f8cab65986d
Ankika Dey <ankikadey@outlook.com>
2024-06-25 09:23:04 +0530

    update codes

--- LeetCode Solutions/Leetcode 75/1137. N-th Tribonacci Number/code.py
+++ LeetCode Solutions/Leetcode 75/1137. N-th Tribonacci Number/code.py
@@ -1,8 +1,8 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        dp = [0, 1, 1]
 
-        for i in range(3, n + 1):
-            dp[i % 3] = sum(dp)
+        t0, t1, t2 = 0, 1, 1
+        for i in range(n):
+            t0, t1, t2 = t1, t2, (t0+t1+t2)
 
-        return dp[n % 3]
+        return t0
