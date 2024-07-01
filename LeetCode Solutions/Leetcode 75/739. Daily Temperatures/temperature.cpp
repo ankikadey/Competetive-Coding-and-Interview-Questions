2762e80017b563de0b019c91feabf0f907b6b076
Ankika Dey <ankikadey@outlook.com>
2024-07-01 12:35:03 +0530

    add temperature.py and temperature.cpp

---
+++ LeetCode Solutions/Leetcode 75/739. Daily Temperatures/temperature.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> dailyTemperatures(vector<int> &temperatures)
+    {
+        vector<int> ans(temperatures.size());
+        stack<int> stack; // a decreasing stack
+
+        for (int i = 0; i < temperatures.size(); ++i)
+        {
+            while (!stack.empty() && temperatures[stack.top()] < temperatures[i])
+            {
+                const int index = stack.top();
+                stack.pop();
+                ans[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        return ans;
+    }
+};
