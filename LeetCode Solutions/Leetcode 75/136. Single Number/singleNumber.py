2f6df3cd851af2b9235e4027f469c1871bb87f2f
Ankika Dey <ankikadey@outlook.com>
2024-07-03 16:51:09 +0530

    add singleNumber.py

---
+++ LeetCode Solutions/Leetcode 75/136. Single Number/singleNumber.py
@@ -0,0 +1,8 @@
+import functools
+import operator
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return functools.reduce(operator.xor, nums, 0)
